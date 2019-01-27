c0a4c4ede10417b5564d1be7d383b5982baaefe7
Aline Ebone <iniebone@gmail.com>
2019-01-27 05:28:36 -0200

    Refactor and add a track trainer

---
+++ src/main/TrackTrainer.java
@@ -0,0 +1,8 @@
+public class TrackTrainer implements Trainer {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+}
--- src/test/TrainerTest.java
+++ src/test/TrainerTest.java
@@ -3,17 +3,20 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class BaseballTrainerTest {
+public class TrainerTest {
 
     Trainer baseballTrainer;
+    Trainer trackTrainer;
 
     @Before
     public void setUp() throws Exception {
         baseballTrainer = new BaseballTrainer();
+        trackTrainer = new TrackTrainer();
     }
 
     @Test
     public void shouldReturnAnExercise() {
         assertTrue(("30 minutes of batting practice").equals(baseballTrainer.getDailyWorkout()));
+        assertTrue(("Run a hard 5k").equals(trackTrainer.getDailyWorkout()));
     }
 }
